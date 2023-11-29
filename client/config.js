a7cfc82c851146342c78cb84cbe386568c6646ab
Michael Hofweller <michael.hofweller@gmail.com>
2023-11-28 18:31:36 -0800

    env

--- client/config.js
+++ client/config.js
@@ -1,7 +1,7 @@
-const API_HOST = process.env.API_HOST || process.env.API_HOST_DEV;
-const API_PORT = process.env.API_PORT || process.env.API_HOST_PORT;
-const WS_HOST = process.env.WS_HOST || process.env.WS_HOST_DEV;
-const WS_PORT = process.env.WS_PORT || process.env.WS_PORT_DEV;
+const API_HOST = process.env.API_HOST || 'localhost';
+const API_PORT = process.env.API_PORT || 8080;
+const WS_HOST = process.env.WS_HOST || 'localhost';
+const WS_PORT = process.env.WS_PORT || 8080;
 
 const API_URL = `http://${API_HOST}:${API_PORT}/graphql`;
 const WS_URL = `ws://${WS_HOST}:${WS_PORT}/graphql`;
--- client/next.config.js
+++ client/next.config.js
@@ -5,13 +5,13 @@ const nextConfig = {
         serverActions: true,
         serverComponentsExternalPackages: ['@logtail/pino', 'pino'],
     },
-    env: {
-        API_HOST_DEV: 'localhost',
-        API_PORT_DEV: '8080',
-        WS_HOST_DEV: 'localhost',
-        WS_PORT_DEV: '8080',
-        BETTERSTACK_TOKEN_CLIENT: 'psgFFB17v9xfYhUYw7EKGGzt',
-    },
+    // env: {
+    //     API_HOST_DEV: 'localhost',
+    //     API_PORT_DEV: '8080',
+    //     WS_HOST_DEV: 'localhost',
+    //     WS_PORT_DEV: '8080',
+    //     BETTERSTACK_TOKEN_CLIENT: 'psgFFB17v9xfYhUYw7EKGGzt',
+    // },
 };
 
 module.exports = nextConfig;
