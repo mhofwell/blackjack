91713ff60f42a53ecc29df919a69db74cc9618e9
Michael Hofweller <michael.hofweller@gmail.com>
2023-11-28 18:42:09 -0800

    force

--- client/config.js
+++ client/config.js
@@ -1,7 +1,7 @@
-const API_HOST = process.env.API_HOST || 'localhost';
-const API_PORT = process.env.API_PORT || 8080;
-const WS_HOST = process.env.WS_HOST || 'localhost';
-const WS_PORT = process.env.WS_PORT || 8080;
+const API_HOST = process.env.API_HOST 
+const API_PORT = process.env.API_PORT 
+const WS_HOST = process.env.WS_HOST
+const WS_PORT = process.env.WS_PORT 
 
 const API_URL = `http://${API_HOST}:${API_PORT}/graphql`;
 const WS_URL = `ws://${WS_HOST}:${WS_PORT}/graphql`;
