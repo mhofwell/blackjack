2a543d16e7e849ce2ae91f467cf59530a0f49bf2
Michael Hofweller <michael.hofweller@gmail.com>
2023-11-30 10:06:00 -0800

    updates

--- client/config.js
+++ client/config.js
@@ -4,7 +4,7 @@ const API_PORT = process.env.API_PORT;
 const WS_PORT = process.env.WS_PORT;
 const API_URL = process.env.API_URL_PUBLIC;
 
-const API_URL_STRING = `http://${API_URL}:${API_PORT}l`;
-const WS_URL_STRING = `ws://${API_URL}:${WS_PORT}`;
+const API_URL_STRING = `http://${API_URL}`;
+const WS_URL_STRING = `ws://${API_URL}`;
 
 export { API_URL_STRING, WS_URL_STRING };
