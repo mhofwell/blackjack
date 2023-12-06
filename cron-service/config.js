db70cfb6fb79c3a866fb8269e148f94f96b93f9d
Michael Hofweller <michael.hofweller@gmail.com>
2023-12-05 23:02:47 -0800

    update

--- cron-service/config.js
+++ cron-service/config.js
@@ -1,42 +1,35 @@
-let HOST,
-    REDIS_PORT,
-    PORT,
-    REDIS_USER,
-    REDIS_HOST,
-    REDIS_PASSWORD,
-    API_PUBLIC_URL,
-    API_PRIVATE_URL,
-    REDIS_URL;
+let HOST, REDIS_PORT, PORT, REDIS_USER, REDIS_HOST, REDIS_PASSWORD, REDIS_URL;
 
 if (process.env.NODE_ENV === 'development') {
     PORT = 9000;
     HOST = 'localhost';
+    API_PORT = 8080;
+    API_URL = `http://${HOST}:8080/graphql`;
     REDIS_HOST = 'localhost';
     REDIS_PORT = 6379;
     REDIS_USER = '';
     REDIS_PASSWORD = '';
     REDIS_URL = 'NONE';
-    API_URL = `http://${HOST}:8080/graphql`;
 } else if (process.env.NODE_ENV === 'production') {
     PORT = process.env.PORT;
     HOST = process.env.HOST;
+    API_PORT = process.env.API_PORT;
+    API_URL = `http://${process.env.API_PRIVATE_URL}:${API_PORT}}/graphql`;
     REDIS_HOST = process.env.REDIS_HOST;
     REDIS_PORT = process.env.REDIS_PORT;
     REDIS_USER = process.env.REDIS_USER;
     REDIS_PASSWORD = process.env.REDIS_PASSWORD;
     REDIS_URL = process.env.REDIS_URL;
-    API_URL = `http://${process.env.API_PRIVATE_URL}:5647/graphql`;
 }
 
 module.exports = {
     PORT,
     HOST,
+    API_PORT,
+    API_URL,
+    REDIS_HOST,
+    REDIS_PORT,
     REDIS_USER,
     REDIS_PASSWORD,
-    REDIS_HOST,
     REDIS_URL,
-    API_PUBLIC_URL,
-    API_PRIVATE_URL,
-    CRON_PORT,
-    API_URL,
 };
