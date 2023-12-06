3858d27590faeb9480f79f8903c46e77ab0d8b52
Michael Hofweller <michael.hofweller@gmail.com>
2023-12-05 16:59:50 -0800

    node env

--- client/config.js
+++ client/config.js
@@ -2,10 +2,8 @@ let API_URL;
 
 if (process.env.NODE_ENV === 'development') {
     API_URL = 'localhost:8080';
-} else if (process.env.NODE_ENV === 'build') {
+} else if (process.env.NODE_ENV === 'production') {
     API_URL = process.env.API_PUBLIC_URL;
-} else if (process.env.NODE_ENG === 'production') {
-    API_URL = process.env.API_PRIVATE_URL;
 }
 
 const API_URL_STRING = `http://${API_URL}/graphql`;
