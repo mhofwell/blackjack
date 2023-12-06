51064ae7adab80e64045be201277ae6c7fbfef81
Michael Hofweller <michael.hofweller@gmail.com>
2023-12-05 17:01:43 -0800

    node env

--- client/config.js
+++ client/config.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV === 'development') {
     API_URL = process.env.API_PUBLIC_URL;
 }
 
-const API_URL_STRING = `http://${API_URL}/graphql`;
+const API_URL_STRING = `https://${API_URL}/graphql`;
 const WS_URL_STRING = `ws://${API_URL}/graphql`;
 
 export { API_URL_STRING, WS_URL_STRING };
