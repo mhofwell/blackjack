c8d5cb936ba119ccbee54054aabd3f24bf292034
Michael Hofweller <michael.hofweller@gmail.com>
2023-12-12 13:57:51 -0800

    cors fixes

--- server/config.js
+++ server/config.js
@@ -25,9 +25,9 @@ if (process.env.NODE_ENV === 'development') {
     REDIS_USER = process.env.REDIS_USER;
     REDIS_PASSWORD = process.env.REDIS_PASSWORD;
     CLIENT_PORT = process.env.CLIENT_PORT;
-    CLIENT_PRIVATE_URL = process.env.CLIENT_PRIVATE_URL;
-    MS_PRIVATE_URL = process.env.MS_PRIVATE_URL;
-    DB_PRIVATE_URL = process.env.DB_PRIVATE_URL;
+    CLIENT_PRIVATE_URL = `https://${process.env.CLIENT_PRIVATE_URL}`;
+    MS_PRIVATE_URL = `https://${process.env.MS_PRIVATE_URL}`;
+    DB_PRIVATE_URL = `https://${process.env.DB_PRIVATE_URL}`;
     PORT = process.env.PORT;
 }
 
