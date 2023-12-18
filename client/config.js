29b33f40ce72e04e5f8a93de9ba80386ac7f4daa
Michael Hofweller <michael.hofweller@gmail.com>
2023-12-18 15:28:24 -0800

    url updates

--- client/config.js
+++ client/config.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === 'development') {
     API_URL = process.env.API_URL;
     WS_URL = process.env.WS_URL;
 } else if (process.env.NODE_ENV === 'production') {
-    API_URL = process.env.API_PUBLIC_URL;
+    API_URL = `https://${process.env.API_PUBLIC_URL}/graphql`;
     WS_URL = `wss://${API_URL}/graphql`;
 }
 
