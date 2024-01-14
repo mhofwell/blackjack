62afbc87b81c68fc4b2d6cd12218dda3829a2049
Michael Hofweller <michael.hofweller@gmail.com>
2024-01-14 14:46:46 -0800

    cron-refactor

--- cron-service/test.js
+++ cron-service/test.js
@@ -1,43 +1,19 @@
-// new updatePlayerGoals.js
+// improvements
+// can we get a list of all the players, and the entry UUID's they belong to? Get that, save it and use it below.
+// replace console.log with logger
 
-// get all player ids that are playing this game week into an array.
-// Poll API for changes to live scores every two minutes.
-// If someone scores, check if we care about that player against ID's in that array.
-// If yes,
-// // get that player
-// // do we have it in cache already?
-// // if no
-// // // store current goal state in Redis cahce for that player
-// // +1 to goals, net_goals of that players stats.
-// // if yes
-// // //
-// Get all entries that have that player
-// // For each entry containing that player
-// // +1 to goals, net_goals on entries with that player.
-// // save entry
+// --
 
-// Re-sort Pool that the entry is in.
-
-// // Fetch pool entries
-// // Entries < 21 total && all 4 players have goals > 0 move into an array.
-// // entries.sort(sortByDesc)
-// // for each entry rank = entry.indexOf(entries) + 1
-// Entries with not A4S and < 21 rank = "-"
-// Entries with > 21 goals, rank = "BUST!"
-
-// // create helper functions for A4S and <21 goal checks.
-
-// we're going to need to compare 2 different array's.
-
-// const array1a = stats[0].a
-// const array1h = stats[0].h
-
+// inputs for the function set to work properly. 
 const kt = '2023-12-30T12:30:00Z';
+const gw = 20;
 
+// this returns the life Gameweek Data
+// this requires the input of the gameweek week.
 async function getLiveGameweekData() {
     try {
         const res = await fetch(
-            'https://fantasy.premierleague.com/api/fixtures?event=20'
+            `https://fantasy.premierleague.com/api/fixtures?event=${gw}`
         );
         const data = await res.json();
         return data;
@@ -55,6 +31,32 @@ function getFixtures(data, kt) {
     }
     return fixtures;
 }
+
+// check for a change in the size of the goal and own goal arrays.
+function checkForGoalEvent() {}
+
+// check if the goal was from a player in a pool entry.
+function checkForValidPlayer() {}
+
+// if we care about the player, create the cached player data array.
+// maybe we should create a db entry for all of the players in all entries so we can quick index that.
+async function cacheGoalData(fixtures) {}
+
+// get the db ppol entries that contain the player who scored
+async function getEntries() {}
+
+// update the pool entries goals and net goals by the number og goals that scored
+// update entry status here as well: RANKED, UNRANKED, LOST
+// save entries
+// return entries (or maybe only pool UUID of the entries to sort)
+async function updateEntries() {}
+
+// Take in pool UUID's that we need to sort
+// if RANKED, sortByDesc net goals, assign # rank, push into array.
+// if UNRANKED, push into array
+// if LOST, push into array
+// push subscription event through with newly sorted pools.
+async function sortPools() {}
 
 async function main() {
     const liveGameweekData = await getLiveGameweekData();
